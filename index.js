35de2aa43be6839b8f8b6e71af634500eef5583a
vadim l <vadimlozinschii@gmail.com>
2023-03-23 06:52:33 -0700

    passed the lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    console.log("The DOM has loaded");
+    const text = document.getElementById("text");
+    text.textContent = "This is really cool!";
+});
+
+console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
+
+// const text = document.getElementById("text");
+// text.textContent = "JavaScript is so cool. It lets me add text to my page programmatically.";
